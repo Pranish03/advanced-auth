0eb4b1ada9bdc2229afae33265fe4ea38080b821
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-23 17:40:58 +0545

    feat: enhance authentication with email verification and password reset functionality

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,10 +2,23 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/drizzle/db";
 import { nextCookies } from "better-auth/next-js";
+import { sendPasswordResetEmail } from "./emails/password-reset-email";
+import { sendVerificationEmail } from "./emails/verification-email";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: true,
+    sendResetPassword: async ({ user, url }) => {
+      await sendPasswordResetEmail({ user, url });
+    },
+  },
+  emailVerification: {
+    autoSignInAfterVerification: true,
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendVerificationEmail({ user, url });
+    },
   },
   socialProviders: {
     google: {
