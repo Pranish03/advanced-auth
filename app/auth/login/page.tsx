fe37c646448e4c716b15c03b22b19b52a764657e
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-24 18:01:36 +0545

    feat: add email verification tab to login page with state management

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -12,12 +12,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInTab from "./_components/sign-in-tab";
 import SignUpTab from "./_components/sign-up-tab";
 import SocialAuthButtons from "./_components/social-auth-buttons";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
+import EmailVerification from "./_components/email-verification";
+
+type Tab = "signin" | "signup" | "email-verification";
 
 export default function LoginPage() {
   const router = useRouter();
+
+  const [email, setEmail] = useState<string>("test@email.com");
+  const [selectedTab, setSelectedTab] = useState<Tab>("email-verification");
 
   useEffect(() => {
     authClient.getSession().then((session) => {
@@ -26,7 +32,11 @@ export default function LoginPage() {
   }, [router]);
 
   return (
-    <Tabs defaultValue="signin" className="mx-auto w-full max-w-md my-10 px-4">
+    <Tabs
+      value={selectedTab}
+      onValueChange={(t) => setSelectedTab(t as Tab)}
+      className="mx-auto w-full max-w-md my-10 px-4"
+    >
       <TabsList>
         <TabsTrigger value="signin">Log in</TabsTrigger>
         <TabsTrigger value="signup">Sign up</TabsTrigger>
@@ -62,6 +72,17 @@ export default function LoginPage() {
           <CardFooter className="grid grid-cols-2 gap-3">
             <SocialAuthButtons />
           </CardFooter>
+        </Card>
+      </TabsContent>
+
+      <TabsContent value="email-verification">
+        <Card>
+          <CardHeader>
+            <CardTitle>Verify your email</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <EmailVerification email={email} />
+          </CardContent>
         </Card>
       </TabsContent>
     </Tabs>
